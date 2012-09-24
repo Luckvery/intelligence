9f7ea5cc70b2ee6118e21625b93217c947bf5602
Russell John-Baptiste <theluckysuccess@gmail.com>
2012-09-24 00:54:00 -0400

    added set_as_active_page helper to contacts_helper.rb

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,9 @@
 module ContactsHelper
+
+
+  def set_as_active_page(currentController, currentAction)
+    if current_page?(:controller => currentController, :action => currentAction)
+        "class='active'"
+    end
+  end
 end
